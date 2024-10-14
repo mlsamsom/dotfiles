7e10288dacdccd64d011cd672a6641c3f1418055
Michael Samsom <mike.samsom@asica.ca>
2024-10-14 10:45:12 -0600

    Update to ^2.5

--- nvchad_config/lua/configs/lspconfig.lua
+++ nvchad_config/lua/configs/lspconfig.lua
@@ -1,31 +1,114 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local lspconfig = require("lspconfig")
+
+-- list of all servers configured.
+lspconfig.servers = {
+    "lua_ls",
+    -- "clangd",
+    -- "gopls",
+    -- "hls",
+    -- "ols",
+    "pyright",
+}
+
+-- list of servers configured with default config.
+local default_servers = {
+    -- "ols",
+    -- "pyright",
+}
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+for _, lsp in ipairs(default_servers) do
+    lspconfig[lsp].setup({
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+    })
+end
+
+-- lspconfig.clangd.setup({
+--     on_attach = function(client, bufnr)
+--         client.server_capabilities.documentFormattingProvider = false
+--         client.server_capabilities.documentRangeFormattingProvider = false
+--         on_attach(client, bufnr)
+--     end,
+--     on_init = on_init,
+--     capabilities = capabilities,
+-- })
+
+-- lspconfig.gopls.setup({
+--     on_attach = function(client, bufnr)
+--         client.server_capabilities.documentFormattingProvider = false
+--         client.server_capabilities.documentRangeFormattingProvider = false
+--         on_attach(client, bufnr)
+--     end,
+--     on_init = on_init,
+--     capabilities = capabilities,
+--     cmd = { "gopls" },
+--     filetypes = { "go", "gomod", "gotmpl", "gowork" },
+--     root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+--     settings = {
+--         gopls = {
+--             analyses = {
+--                 unusedparams = true,
+--             },
+--             completeUnimported = true,
+--             usePlaceholders = true,
+--             staticcheck = true,
+--         },
+--     },
+-- })
+
+-- lspconfig.hls.setup({
+--     on_attach = function(client, bufnr)
+--         client.server_capabilities.documentFormattingProvider = false
+--         client.server_capabilities.documentRangeFormattingProvider = false
+--         on_attach(client, bufnr)
+--     end,
+--
+--     on_init = on_init,
+--     capabilities = capabilities,
+-- })
+
+lspconfig.pyright.setup({
     on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
-  }
-end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- python
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"python"},
+    settings = {
+        python = {
+            analysis = {
+                typeCheckingMode = "off",
+            },
+        },
+    },
 })
 
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = false, -- Disable all diagnostics from lua_ls
+                -- globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+                    vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+                    vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+                    "${3rd}/love2d/library",
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+})
--- nvchad_config/lua/plugins/init.lua
+++ nvchad_config/lua/plugins/init.lua
@@ -1,87 +1,145 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
-  },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.treesitter")
+        end,
+    },
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server", "stylua",
-        "html-lsp", "css-lsp" , "prettier",
-        "pyright", "pyright", "rust-analyser",
-        "black", "ruff"
-      },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
     },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = {"python"},
-    opts = function()
-      return require "configs.null-ls"
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim", "lua", "vimdoc",
-       "html", "css"
-      },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
     },
-  },
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function ()
-      vim.g.rustfmt_autosave = 1
-    end
-  },
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = function ()
-      return require "configs.rust-tools"
-    end,
-    config = function (_, opts)
-      require("rust-tools").setup(opts)
-    end
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    keys = {
-      {
-        "<leader>lg",
-        "<cmd> LazyGit<CR>",
-        mode = "n",
-        desc = "LazyGit",
-      },
-      {
-        "<leader>gg",
-        "<cmd> LazyGit<CR>",
-        mode = "n",
-        desc = "LazyGit",
-      },
+
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
     },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
     },
-  },
+
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require("configs.conform")
+        end,
+    },
+
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        keys = {
+            {
+                "<leader>lg",
+                "<cmd> LazyGit<CR>",
+                mode = "n",
+                desc = "LazyGit",
+            },
+            {
+                "<leader>gg",
+                "<cmd> LazyGit<CR>",
+                mode = "n",
+                desc = "LazyGit",
+            },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^5", -- Recommended
+        lazy = false, -- This plugin is already lazy
+        ft = "rust",
+        config = function()
+            local mason_registry = require("mason-registry")
+            local codelldb = mason_registry.get_package("codelldb")
+            local extension_path = codelldb:get_install_path() .. "/extension/"
+            local codelldb_path = extension_path .. "adapter/codelldb"
+            local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+            -- If you are on Linux, replace the line above with the line below:
+            -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+            local cfg = require("rustaceanvim.config")
+
+            vim.g.rustaceanvim = {
+                dap = {
+                    adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+                },
+            }
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end,
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = function()
+            require("dapui").setup()
+        end,
+    },
+    {
+        "saecki/crates.nvim",
+        ft = { "toml" },
+        config = function()
+            require("crates").setup({
+                completion = {
+                    cmp = {
+                        enabled = false,
+                    },
+                },
+            })
+            require("cmp").setup.buffer({
+                sources = { { name = "crates" } },
+            })
+        end,
+    },
 }
